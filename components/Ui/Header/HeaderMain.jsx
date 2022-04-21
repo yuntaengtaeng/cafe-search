5d2b4cc5e299eac48c6c6d95015c238bfe57fef0
yuntaengtaeng <k76253218@gmail.com>
2022-04-21 21:45:41 +0900

    Ui HeaderMain 컴포넌트 추가
    - 메인 지도에서 사용하는 헤더 컴포넌트

--- components/Ui/Header/HeaderMain.jsx
+++ components/Ui/Header/HeaderMain.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
+import { VscSearch } from 'react-icons/vsc';
 
 const Header = styled.header`
   width: 100vw;
   height: 3.5rem;
   background-color: #5dfda5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+
+  h3 {
+    margin: 0 1rem 0 0;
+  }
 `;
 
-const HeaderMain = () => {
-  return <Header></Header>;
+const HeaderMain = ({ address }) => {
+  return (
+    <Header>
+      <h3>{address}</h3>
+      <VscSearch size={24} color="white"></VscSearch>
+    </Header>
+  );
 };
 
 export default HeaderMain;
